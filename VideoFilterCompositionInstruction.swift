19123231101b9512937cb8d3cc63e2aa26246891
Domenico <admin@jojodmo.com>
2016-11-09 21:02:54 -0800

    Update to Swift 3

--- VideoFilterCompositionInstruction.swift
+++ VideoFilterCompositionInstruction.swift
@@ -18,12 +18,14 @@ import Foundation
 
 class VideoFilterCompositionInstruction : AVMutableVideoCompositionInstruction{
     
+   // For implementation in Swift 2.x, look at the history of this file at
+   // https://github.com/jojodmo/VideoFilterExporter/blob/1d506238a445b6684ef40d2701419cc01158331e/VideoFilterCompositionInstruction.swift
+   
     let trackID: CMPersistentTrackID
     let filters: [CIFilter]
     let context: CIContext
     
-    override var passthroughTrackID: CMPersistentTrackID{get{return self.trackID}}
-    override var requiredSourceTrackIDs: [NSValue]{get{return [NSNumber(integer: Int(self.trackID))]}}
+    override var requiredSourceTrackIDs: [NSValue]{get{return [NSNumber(value: Int(self.trackID))]}}
     override var containsTweening: Bool{get{return false}}
     
     init(trackID: CMPersistentTrackID, filters: [CIFilter], context: CIContext){
